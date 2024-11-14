6c2dcbc1ebd132d7de6176204e7df644069d14da
PauloAndreCR <plandrecastilho@hotmail.com>
2024-11-13 23:51:21 -0300

    update

--- trabalho final web2/script3.js
+++ trabalho final web2/script3.js
@@ -4,35 +4,78 @@ const pictureImageTxt = "Choose an image";
 pictureImage.innerHTML = pictureImageTxt;
 
 inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target;
-  const file = inputTarget.files[0];
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
 
-  if (file) {
-    const reader = new FileReader();
+    if (file) {
+        const reader = new FileReader();
 
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target;
+        reader.addEventListener("load", function (e) {
+            const readerTarget = e.target;
 
-      const img = document.createElement("img");
-      img.src = readerTarget.result;
-      img.classList.add("picture__img");
+            const img = document.createElement("img");
+            img.src = readerTarget.result;
+            img.classList.add("picture__img");
 
-      pictureImage.innerHTML = "";
-      pictureImage.appendChild(img);
-    });
+            pictureImage.innerHTML = "";
+            pictureImage.appendChild(img);
+        });
 
-    reader.readAsDataURL(file);
-  } else {
-    pictureImage.innerHTML = pictureImageTxt;
-  }
+        reader.readAsDataURL(file);
+    } else {
+        pictureImage.innerHTML = pictureImageTxt;
+    }
 });
 
-function mensage(){
-  alert('Você cadastou este imóvel')
-  recarregarPagina();
+function mensage() {
+    alert("Você cadastou este imóvel");
+    recarregarPagina();
 }
-
 
 function recarregarPagina() {
-  location.reload();
+    location.reload();
 }
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+    this.navLinks.forEach((link, index) => {
+        link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+            }s`);
+    });
+    }
+
+    handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+    }
+
+    addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+    if (this.mobileMenu) {
+        this.addClickEvent();
+    }
+    return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
